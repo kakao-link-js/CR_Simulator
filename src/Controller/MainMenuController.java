b27da5ebe28281dd38d3417397f27f6deec3ae92
jaesung <cjs6027@naver.com>
2019-11-12 13:23:33 +0900

    Make MyLectureScorePanel in LectureListView

--- src/Controller/MainMenuController.java
+++ src/Controller/MainMenuController.java
@@ -2,6 +2,8 @@ package Controller;
 
 import javax.swing.JFrame;
 
+import Model.ClassManagement;
+import Model.LectureVO;
 import View.*;
 
 public class MainMenuController {
@@ -11,6 +13,8 @@ public class MainMenuController {
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setResizable(false); 
 		
+		addTestData();
+		
 		LectureListView LLV = new LectureListView(true);
 		
 
@@ -19,5 +23,29 @@ public class MainMenuController {
 		frame.pack();
 		frame.setVisible(true);
 	}
+	
+	//테스트 데이터 생성용
+		public static void addTestData() {
+			LectureVO new1 = new LectureVO("0","0","0","0","0","0","0","1","0","0","0");
+			ClassManagement.getInstance().getInterested().add(new1);
+			LectureVO new2 = new LectureVO("0","0","0","0","0","0","0","2","0","0","0");
+			ClassManagement.getInstance().getInterested().add(new2);
+			LectureVO new3 = new LectureVO("0","0","0","0","0","0","0","3","0","0","0");
+			ClassManagement.getInstance().getInterested().add(new3);
+			LectureVO new4 = new LectureVO("0","0","0","0","0","0","0","4","0","0","0");
+			ClassManagement.getInstance().getInterested().add(new4);
+			LectureVO new5 = new LectureVO("0","0","0","0","0","0","0","5","0","0","0");
+			ClassManagement.getInstance().getInterested().add(new5);
+			LectureVO new6 = new LectureVO("0","0","0","0","0","0","0","6","0","0","0");
+			ClassManagement.getInstance().getInterested().add(new6);
+			LectureVO new7 = new LectureVO("0","0","0","0","0","0","0","7","0","0","0");
+			ClassManagement.getInstance().getInterested().add(new7);
+			LectureVO new8 = new LectureVO("0","0","0","0","0","0","0","8","0","0","0");
+			ClassManagement.getInstance().getInterested().add(new8);
+			LectureVO new9 = new LectureVO("0","0","0","0","0","0","0","9","0","0","0");
+			ClassManagement.getInstance().getInterested().add(new9);
+			LectureVO new10 = new LectureVO("0","0","0","0","0","0","0","0","0","0","0");
+			ClassManagement.getInstance().getInterested().add(new10);
+		}
 
 }
--- src/View/LectureListView.java
+++ src/View/LectureListView.java
@@ -7,19 +7,23 @@ import Model.*;
 
 public class LectureListView extends JPanel {
 	JPanel searchListPanel; //필터링 한 출력 Panel
+	JPanel statusPanel; //정보표현용 Panel
 	JPanel myLecturePanel; //내 수강신청 과목 Panel
+	
+	//표 출력을 위한 Instance
 	JTable searchListTable;
 	JTable myLectureTable;
 	JScrollPane searchListPane;
 	JScrollPane myLecturePane;
 	
-	private int width = 1500; //동적 사이즈 조절을 위한 사이즈 변수
-	private int height = 1000;
+	JLabel lblScore;
+	
+	
+	private int width = 1000; //동적 사이즈 조절을 위한 사이즈 변수
+	private int height = 700;
 	private int margin = 10;
 	
 	String header[] = {"개설대학","개설학과전공","학수번호","분반","교과목명","이수구분","학년","학점","요일 및 강의시간","강의실","교수"};
-	
-	ArrayList<LectureVO> testData = new ArrayList<LectureVO>(); //test데이터 저장용.
 	
 	public LectureListView(boolean flag) {
 		initPrimaryPanel();
@@ -27,69 +31,68 @@ public class LectureListView extends JPanel {
 		setLectureListPanel();
 		
 		setMyLecturePanel();
-	}
-	
-	//테스트 데이터 생성용
-	public void addTestData() {
-		LectureVO new1 = new LectureVO("0","0","0","0","0","0","0","0","0","0","0");
-		testData.add(new1);
-		LectureVO new2 = new LectureVO("0","0","0","0","0","0","0","0","0","0","0");
-		testData.add(new2);
-		LectureVO new3 = new LectureVO("0","0","0","0","0","0","0","0","0","0","0");
-		testData.add(new3);
-		LectureVO new4 = new LectureVO("0","0","0","0","0","0","0","0","0","0","0");
-		testData.add(new4);
-		LectureVO new5 = new LectureVO("0","0","0","0","0","0","0","0","0","0","0");
-		testData.add(new5);
-		LectureVO new6 = new LectureVO("0","0","0","0","0","0","0","0","0","0","0");
-		testData.add(new6);
-		LectureVO new7 = new LectureVO("0","0","0","0","0","0","0","0","0","0","0");
-		testData.add(new7);
-		LectureVO new8 = new LectureVO("0","0","0","0","0","0","0","0","0","0","0");
-		testData.add(new8);
-		LectureVO new9 = new LectureVO("0","0","0","0","0","0","0","0","0","0","0");
-		testData.add(new9);
-		LectureVO new10 = new LectureVO("0","0","0","0","0","0","0","0","0","0","0");
-		testData.add(new10);
-	}
+		
+		setStatusPanel();
+	} //LectureListView(boolean flag)
 	
 	//PrimaryPanel 초기화
 	public void initPrimaryPanel() {
 		setPreferredSize(new Dimension(width,height)); 
 		setBackground(Color.white);
 		setLayout(null);
-	}
+	}//initPrimaryPanel()
 	
 	//LectureListPanel 초기화
 	public void setLectureListPanel() {
 		searchListPanel = new JPanel();
-		searchListPanel.setBounds(margin,margin,width-margin*2,(int) (height*0.7-margin*1.5));
+		searchListPanel.setBounds(margin,margin,width-margin*2,(int) (height*0.6-margin*1.5));
 		searchListPanel.setBackground(Color.WHITE);
 		searchListPanel.setLayout(null);
 		add(searchListPanel);
 		
-		addTestData(); //테스트용
-		
-		searchListTable = new JTable(makeInsertData(testData),header);
+		searchListTable = new JTable(makeInsertData(ClassManagement.getInstance().getInterested()),header);
 		searchListPane = new JScrollPane(searchListTable);
 		searchListPane.setBounds(0,0,searchListPanel.getWidth(),searchListPanel.getHeight());
 		searchListPanel.add(searchListPane);
-	}
+	} //setLectureListPanel()
 	
 	//MyLecturePanel 초기화
 	public void setMyLecturePanel() {
 		myLecturePanel = new JPanel();
-		myLecturePanel.setBounds(margin,(int) (height*0.7+margin*0.5),width-margin*2,(int) (height*0.3-margin*1.5));
+		myLecturePanel.setBounds(margin,(int) (height*0.65+margin*0.5),width-margin*2,(int) (height*0.35-margin*1.5));
 		myLecturePanel.setBackground(Color.CYAN);
 		myLecturePanel.setLayout(null);
 		add(myLecturePanel);
 		
-		addTestData(); //테스트용
-		
-		myLectureTable = new JTable(makeInsertData(testData),header);
+		myLectureTable = new JTable(makeInsertData(ClassManagement.getInstance().getInterested()),header);
 		myLecturePane = new JScrollPane(myLectureTable);
 		myLecturePane.setBounds(0,0,myLecturePanel.getWidth(),myLecturePanel.getHeight());
 		myLecturePanel.add(myLecturePane);
+	} //setMyLecturePanel()
+	
+	public void setStatusPanel() {
+		statusPanel = new JPanel();
+		statusPanel.setBounds(margin,(int)(height*0.6),width-margin*2,(int)(height*0.05));
+		statusPanel.setBackground(Color.white);
+		statusPanel.setLayout(null);
+		add(statusPanel);
+		
+		lblScore = new JLabel("My Score : ");
+		lblScore.setBounds(10, statusPanel.getHeight() / 2, width-margin*2, (int)(statusPanel.getHeight()*0.5));
+		lblScore.setFont(new Font("Verdana",Font.BOLD,14));
+		lblScore.setForeground(Color.BLACK);
+		
+		SetScore();
+		
+		statusPanel.add(lblScore);
+	} //setStatusPanel()
+	
+	public void SetScore() {
+		int sums = 0;
+		for(int i = 0 ; i < myLectureTable.getRowCount(); i++) {
+			sums += Integer.parseInt((String) myLectureTable.getValueAt(i, 7));
+		}
+		lblScore.setText("My Scroe : " + sums);
 	}
 	
 	//문장 2차원 배열을 돌려주는 메소드
@@ -99,6 +102,6 @@ public class LectureListView extends JPanel {
 		for(int i = 0 ; i < lists.size();i++)
 			output[i] = lists.get(i).makeStringArray();
 		return output;
-	}
+	} //makeInsertData(ArrayList<LectureVO> lists)
 	
 }
