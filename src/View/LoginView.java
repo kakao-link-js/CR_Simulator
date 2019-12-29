39d87e6de7d6dfd7b06a8a013cb8edda508fcb54
msmn1729 <msmn1729@gmail.com>
2019-12-30 02:50:01 +0900

    actionListener add

--- src/View/LoginView.java
+++ src/View/LoginView.java
@@ -1,11 +1,13 @@
 package View;
 
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.*;
 import javax.swing.*;
 import javax.swing.table.*;
 
-import org.apache.commons.math3.analysis.function.Add;
+//import org.apache.commons.math3.analysis.function.Add;
 
 import Controller.LectureListController;
 import Controller.LectureListController.CellRenderer;
@@ -13,8 +15,6 @@ import Model.*;
 import common.DesignConstants;
 
 public class LoginView extends JPanel{
- //	LoginViewController LVC;
-	
 	JPanel imagePanel; //상단에 삽입할 이미지 패널
 	JPanel loginPanel; //하단에 삽입할 로그인 패널
 
@@ -27,17 +27,16 @@ public class LoginView extends JPanel{
 
 	//생성자
 	public LoginView() {
-		
+
 		//500x360
 		width = 500;
 		height = 360;
-		
 		margin = (int)(width * 0.01);
-		
+
 		setPreferredSize(new Dimension(width, height));
 		setBackground(Color.white);
 		setLayout(null);
-		
+
 		setImagePanel();
 		setComponent();
 	}
@@ -45,33 +44,33 @@ public class LoginView extends JPanel{
 	public void setImagePanel() {
 		imagePanel = new JPanel();
 		imagePanel.setBounds(0, 0, this.width, (int)(this.height*0.5));
-		
+
 		ImageIcon image = new ImageIcon("Images/sejong.png");
 		Image sizeConversation = image.getImage();
-		
+
 		double length = (int)(height*0.5)-margin*2;
 		Image results = sizeConversation.getScaledInstance((int)length, (int)length, java.awt.Image.SCALE_SMOOTH);
 		ImageIcon sizeImg = new ImageIcon(results);
 		JLabel label = new JLabel(sizeImg);
 		imagePanel.add(label);
 		imagePanel.setBackground(Color.white);
-		
+
 		add(imagePanel);
 	}
 
 	//component을 구성하는 메소드
 	public void setComponent() {
-		
+
 		int x = (int) (width*0.1),y =(int) (height*0.2) , width , height ;
 
 		width = (int) (this.width * 0.12);
 		height = (int)(this.height * 0.2*0.5);
-		
+
 		//로그인 패널
 		loginPanel = new JPanel();
 		loginPanel.setBounds((int)(width*0.5), imagePanel.getHeight()+margin*4, this.width, (int)(this.height*0.5));
 		loginPanel.setLayout(null);
-		
+
 		//라벨
 		JLabel ID = new JLabel("ID");
 		JLabel PW = new JLabel("PW");
@@ -93,9 +92,9 @@ public class LoginView extends JPanel{
 		y = ID.getY();
 		width = (int)(this.width*0.39);
 		height = ID.getHeight();
-		
+
 		idTextField = new JTextField();
-		pwTextField = new JTextField();
+		pwTextField = new JPasswordField();
 
 		idTextField.setBounds(x, y, width, height);
 		pwTextField.setBounds(x, y+idTextField.getHeight()+margin, width, height);
@@ -108,7 +107,6 @@ public class LoginView extends JPanel{
 		searchIdBtn = new JButton("ID 찾기");
 		signUpBtn = new JButton("회원 가입");
 
-		System.out.println(height);
 		loginBtn.setBounds(idTextField.getX()+idTextField.getWidth()+margin*2, idTextField.getY(),
 				(int)(this.width*0.19), idTextField.getHeight()+pwTextField.getHeight()+margin);
 		searchIdBtn.setBounds(idTextField.getX(), idTextField.getY()+idTextField.getHeight()+pwTextField.getHeight()+margin*2,
@@ -118,11 +116,52 @@ public class LoginView extends JPanel{
 		loginPanel.add(loginBtn);
 		loginPanel.add(searchIdBtn);
 		loginPanel.add(signUpBtn);
-		
+
 		loginPanel.setBackground(Color.white);
 		add(loginPanel);
 
+
+		//***********************************************//
+		//회원가입 리스너
+		ActionListener listener = new ActionListener()
+		{
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				//ClassManager.getInstance().getMainMenuController().comeToMain();
+				//회원가입창을 새창에 띄움 
+				JFrame frame = new JFrame();
+				frame.setBounds(770,220,485,570);
+				frame.add(new SignUpView());
+				frame.setVisible(true);
+			}
+
+		};
+		signUpBtn.addActionListener(listener);
+		
+		//로그인버튼 리스너
+		ActionListener id_pw_Listener = new ActionListener()
+		{
+			public void actionPerformed(ActionEvent e) {
+				String id = idTextField.getText();
+				char[] pass = ((JPasswordField) pwTextField).getPassword();
+				String password = new String(pass);
+				JOptionPane.showMessageDialog(null, "ID : "+id+"\nPW : "+password);
+			}
+		};
+		loginBtn.addActionListener(id_pw_Listener);
+		
+		//id찾기버튼 리스너
+		ActionListener id_Listener = new ActionListener()
+		{
+			public void actionPerformed(ActionEvent e) {
+				String id = idTextField.getText();
+				JOptionPane.showMessageDialog(null, "ID찾기");
+			}
+		};
+		searchIdBtn.addActionListener(id_Listener);
 	}
+
 
 
 }
--- src/View/SignUpView.java
+++ src/View/SignUpView.java
@@ -1,5 +1,164 @@
 package View;
 
-public class SignUpView {
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.*;
+import javax.swing.*;
+import javax.swing.table.*;
 
+//import org.apache.commons.math3.analysis.function.Add;
+
+import Controller.LectureListController;
+import Controller.LectureListController.CellRenderer;
+import Model.*;
+import common.DesignConstants;
+
+public class SignUpView extends JPanel{
+	//회원가입 창을 위한 Instance
+	JPanel signUpPanel; //회원가입 패널
+	private int width, height, margin; //기본으로 넣을 크기
+	private JButton duChkBtn; //중복체크 버튼
+	private JButton signUpBtn; //회원가입 버튼
+	private JTextField idTextField, pwTextField;
+	private JTextField nameTextField; //이름 
+	private JTextField phoneTextField; //휴대전화
+	private JTextField dobTextField; //생년월일
+
+	public SignUpView()
+	{
+		//470x570
+		width = 470;
+		height = 570;
+		margin = (int)(width * 0.01);
+
+		setPreferredSize(new Dimension(width, height));
+		setBackground(Color.WHITE);
+		setLayout(null);
+
+		setSignUpPanel();
+	}
+
+	//그룹화
+	private void setSignUpPanel() {
+		int x, y;
+
+		signUpPanel = new JPanel();
+		signUpPanel.setBounds(0, 0, width, height);
+		signUpPanel.setLayout(null);
+
+		//label
+		x = 0;
+		y = (int)(height * 0.15);
+		width = (int)(width * 0.2);
+		height = (int)(height * 0.08);
+
+		JLabel ID = new JLabel("ID(학번)");
+		JLabel PW = new JLabel("비밀번호");
+		JLabel name = new JLabel("이름");
+		JLabel phone = new JLabel("휴대전화");
+		JLabel DOB = new JLabel("생년월일");
+
+		ID.setBounds(x+margin*6, y, width, height);
+		ID.setFont(new Font(DesignConstants.HANGUL_FONT, Font.BOLD + Font.PLAIN, 20));
+		ID.setHorizontalAlignment(SwingConstants.CENTER);
+		ID.setLayout(null);
+
+		PW.setBounds(x+margin*6, ID.getY()+ID.getHeight()+margin*6, width, height);
+		PW.setFont(new Font(DesignConstants.HANGUL_FONT, Font.BOLD + Font.PLAIN, 20));
+		PW.setHorizontalAlignment(SwingConstants.CENTER);
+		PW.setLayout(null);
+
+		name.setBounds(x+margin*6, PW.getY()+PW.getHeight()+margin*6, width, height);
+		name.setFont(new Font(DesignConstants.HANGUL_FONT, Font.BOLD + Font.PLAIN, 20));
+		name.setHorizontalAlignment(SwingConstants.CENTER);
+		name.setLayout(null);
+
+		phone.setBounds(x+margin*6, name.getY()+name.getHeight()+margin*6, width, height);
+		phone.setFont(new Font(DesignConstants.HANGUL_FONT, Font.BOLD + Font.PLAIN, 20));
+		phone.setHorizontalAlignment(SwingConstants.CENTER);
+		phone.setLayout(null);
+
+		DOB.setBounds(x+margin*6, phone.getY()+phone.getHeight()+margin*6, width, height);
+		DOB.setFont(new Font(DesignConstants.HANGUL_FONT, Font.BOLD + Font.PLAIN, 20));
+		DOB.setHorizontalAlignment(SwingConstants.CENTER);
+		DOB.setLayout(null);
+
+		signUpPanel.add(ID);
+		signUpPanel.add(PW);
+		signUpPanel.add(name);
+		signUpPanel.add(phone);
+		signUpPanel.add(DOB);
+
+		//TextField
+		x = ID.getWidth()+margin*10;
+		y =	ID.getY();
+		width = (int)(ID.getWidth()*2.5);
+		height = ID.getHeight();
+
+		idTextField = new JTextField();
+		pwTextField = new JTextField();
+		nameTextField = new JTextField();
+		phoneTextField = new JTextField();
+		dobTextField = new JTextField();
+
+		idTextField.setBounds(x, y, width, height);
+		pwTextField.setBounds(x, y+idTextField.getHeight()+margin*6, width, height);
+		nameTextField.setBounds(x, PW.getY()+idTextField.getHeight()+margin*6, width, height);
+		phoneTextField.setBounds(x, name.getY()+idTextField.getHeight()+margin*6, width, height);
+		dobTextField.setBounds(x, phone.getY()+idTextField.getHeight()+margin*6, width, height);
+
+		signUpPanel.add(idTextField);
+		signUpPanel.add(pwTextField);
+		signUpPanel.add(nameTextField);
+		signUpPanel.add(phoneTextField);
+		signUpPanel.add(dobTextField);
+
+		//Button
+		duChkBtn = new JButton("중복");
+		signUpBtn = new JButton("회원 가입");
+		duChkBtn.setBounds(idTextField.getX()+idTextField.getWidth()+margin*2, ID.getY(),
+				ID.getY(), ID.getHeight());
+		signUpBtn.setBounds((int) (idTextField.getX()*1.4), DOB.getY()+DOB.getHeight()+margin*10,
+				ID.getWidth(), ID.getHeight());
+
+		signUpPanel.add(duChkBtn);
+		signUpPanel.add(signUpBtn);
+		
+		signUpPanel.setBackground(Color.WHITE);
+		add(signUpPanel);
+
+
+		/**************************************************/
+		//액션리스너
+		//중복체크버튼 리스너
+		ActionListener duChkListener = new ActionListener()
+		{
+			public void actionPerformed(ActionEvent e) {
+				String id = idTextField.getText();
+				JOptionPane.showMessageDialog(null, "ID : "+id);
+			}
+		};
+		duChkBtn.addActionListener(duChkListener);
+
+		//회원가입 리스너
+		ActionListener signUpListener = new ActionListener()
+		{
+			public void actionPerformed(ActionEvent e) {
+				String id = idTextField.getText();
+				String pw = pwTextField.getText();
+				String name = nameTextField.getText();
+				String phone = phoneTextField.getText();
+				String dob = dobTextField.getText();
+				String info;
+				info = "ID : "+id+"\nPW : "+pw+"\nname : "+name
+						+"\nphone : "+phone+"\ndob : "+dob;
+				JOptionPane.showMessageDialog(null, info);
+			}
+		};
+		signUpBtn.addActionListener(signUpListener);
+
+
+
+	}
 }
